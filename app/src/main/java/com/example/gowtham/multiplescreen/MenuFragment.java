2948e3f646e8bc2e7a77f28e0824062d27a0e34f
gowtham <gowtham.ece19@gmail.com>
2015-08-18 08:08:22 +0530

    reverted file for test

--- app/src/main/java/com/example/gowtham/multiplescreen/MenuFragment.java
+++ app/src/main/java/com/example/gowtham/multiplescreen/MenuFragment.java
@@ -3,7 +3,6 @@ package com.example.gowtham.multiplescreen;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +16,6 @@ ListView listView;
     ArrayAdapter<String> adapter;
     String menu[];
     Personselected personselected;
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
          View view=inflater.inflate(R.layout.menu_fragment,container,false);
